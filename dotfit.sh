4db1ebb661eefbafaa95d229d17086220f5a4b23
hamazarashi <i16hamadat@gmail.com>
2018-08-28 22:43:32 +0900

    update dotfiles @ 2018年 8月28日 火曜日 22時43分32秒 JST

--- dotfit.sh
+++ dotfit.sh
@@ -1,8 +1,6 @@
 git pull origin
-cp ./.zshrc ~ &&
-cp ./.zprofile ~ &&
-cp ./.vimrc ~ &&
-cd ~
-shopt -s expand_alias
-source ~/.zshrc
-cd ~/.dotfiles
+cp ./.zshrc ~ 
+cp ./.zprofile ~ 
+cp ./.vimrc ~
+exec zsh
+
